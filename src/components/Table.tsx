a9dd4c7e5a89c7cad854f03c1526ab735f73365d
AmsBarros <amandabarros02@msn.com>
2023-10-01 15:36:30 -0300

    Requisito 5 (falta refatorar)

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,5 +1,44 @@
+import { useSelector } from 'react-redux';
+import { GlobalState } from '../types';
+
 function Table() {
-  return <div>Table</div>;
+  const { expenses } = useSelector((state: GlobalState) => state.wallet);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </thead>
+      <tbody>
+        {expenses.map((item) => (
+          <tr key={ item.id }>
+            <td>{item.description}</td>
+            <td>{item.tag}</td>
+            <td>{item.method}</td>
+            <td>{Number(item.value).toFixed(2)}</td>
+            <td>{item.exchangeRates[item.currency].name}</td>
+            <td>{Number(item.exchangeRates[item.currency].ask).toFixed(2)}</td>
+            <td>{(Number(item.value) * Number(item.exchangeRates[item.currency].ask)).toFixed(2)}</td>
+            <td>Real</td>
+            <td>
+              <button>Editar</button>
+              <button>Excluir</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default Table;
