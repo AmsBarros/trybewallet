079e900c8690f463dba6766aeffda194e5796a52
AmsBarros <amandabarros02@msn.com>
2023-10-05 10:28:34 -0300

    Iniciando CSS

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -39,27 +39,48 @@ function Login() {
   };
 
   return (
-    <div>
-      <input
-        type="email"
-        placeholder="E-mail"
-        data-testid="email-input"
-        value={ email }
-        onChange={ handleEmailChange }
-      />
-      <input
-        type="password"
-        placeholder="Senha"
-        data-testid="password-input"
-        value={ password }
-        onChange={ handlePasswordChange }
-      />
-      <button
-        onClick={ handleSubmit }
-        disabled={ !isFormValid }
-      >
-        Entrar
-      </button>
+    <div
+      className="min-h-screen
+        flex items-center justify-center bg-cover bg-no-repeat bg-center"
+      style={ { backgroundImage: 'url(\'/src/images/background.png\')' } }
+    >
+      <div className="bg-white shadow-md rounded pt-8 pb-10 sm:rounded-lg sm:px-14">
+        <h2 className="text-2xl text-center mb-5">
+          <img
+            src="/src/images/login-logo.png"
+            alt="Ícone de usuário"
+            className="inline mb-2 mr-4"
+          />
+          Trybe
+          <span className="font-bold text-green-500">Wallet</span>
+        </h2>
+        <input
+          className="mb-4 p-2 border rounded block w-full
+            focus:outline-none focus:border-green-700 focus:ring-1 focus:ring-green-700"
+          type="email"
+          placeholder="E-mail"
+          data-testid="email-input"
+          value={ email }
+          onChange={ handleEmailChange }
+        />
+        <input
+          className="mb-4 p-2 border rounded block w-full
+            focus:outline-none focus:border-green-700 focus:ring-1 focus:ring-green-700"
+          type="password"
+          placeholder="Senha"
+          data-testid="password-input"
+          value={ password }
+          onChange={ handlePasswordChange }
+        />
+        <button
+          className="bg-green-500
+            hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full"
+          onClick={ handleSubmit }
+          disabled={ !isFormValid }
+        >
+          Entrar
+        </button>
+      </div>
     </div>
   );
 }
